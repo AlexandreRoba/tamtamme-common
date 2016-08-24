985a1a9e8a1c2e2463259af5eebecf0f933db86b
Alexandre Roba <alexandre.roba@arobanet.be>
2016-08-24 20:08:18 +0200

    Removed command id from constructor

--- src/factory/command.js
+++ src/factory/command.js
@@ -3,15 +3,14 @@ let uuid = require('uuid');
 /**
  * Create a command
  * @param {string} commandName - The command name
- * @param {string} {commandId} - A command id
  * @param {object} payload - The command paylod
  * @constructor {Command}
  */
-function Command(commandName,commandId,payload){
+function Command(commandName,payload){
     if(!commandName)
         throw new Error('The command name cannot be null or empty');
     this.commandName = commandName;
-    this.commandId = commandId || uuid.v1();
+    this.commandId = uuid.v1();
     this.payload = payload || {};
 }
 

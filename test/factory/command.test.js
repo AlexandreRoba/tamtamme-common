bf8a72e8181733e418b2196fc1715b58b6f10658
Alexandre Roba <alexandre.roba@arobanet.be>
2016-08-27 22:00:07 +0200

    Making the test pass again

--- test/factory/command.test.js
+++ test/factory/command.test.js
@@ -8,16 +8,31 @@ describe('Command',()=>{
                expect(()=>{let sut = new Command();}).to.throw(Error);
            })
        });
-       describe('When called with no commandId',()=>{
+       describe('When called with no command Id',()=>{
            let sut = new Command('name');
            it('should set the name',()=>{
-              expect(sut.commandName).to.be.equal('name');
+              expect(sut.name).to.be.equal('name');
            });
            it('should generate a command id',()=>{
-               expect(sut.commandId).to.not.equal(null);
+               expect(sut.id).to.not.equal(null);
            });
            it('should set the payload to an empty object',()=>{
                expect(sut.payload).to.deep.equal({});
+           })
+       });
+       describe('when called with an aggregate id and name',()=>{
+           it('should set the aggregate id and name',(done)=>{
+               let sut = new Command('command',{id:1,name:'aggregate'},{});
+               expect(sut.aggregate.id).to.equal(1);
+               expect(sut.aggregate.name).to.equal('aggregate');
+               done();
+           })
+       })
+       describe('when called without aggregate info but a payload',()=>{
+           it('should get the paylod from the second parameter',(done)=>{
+               let sut = new Command('command',{param1:'value1'});
+               expect(sut.payload).to.deep.equal({param1:'value1'});
+               done();
            })
        })
    })

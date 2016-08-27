b16a288f133f3003d53f3af7da3dd7903a775994
Alexandre Roba <alexandre.roba@arobanet.be>
2016-08-27 23:31:25 +0200

    Exported the NoLogger

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let requireEnvAsObject = require('./src/env');
 let eventDefinition = require('./src/definition/event');
 let commandDefinition = require('./src/definition/command');
 let Command = require("./src/factory/command");
+let NoLogger = require('./src/logger').NoLogger;
 
 module.exports = {
     MessageBus : MessageBus,
     requireEnvAsObject : requireEnvAsObject,
     commandDefinition : commandDefinition,
     eventDefinition: eventDefinition,
-    Command:Command
+    Command:Command,
+    NoLogger:NoLogger,
 };

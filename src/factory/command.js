4b92b82ae18d6828e71354ff3e9252de07d38d8e
Alexandre Roba <alexandre.roba@arobanet.be>
2016-08-28 22:04:39 +0200

    Added the meta to the command factory

--- src/factory/command.js
+++ src/factory/command.js
@@ -5,19 +5,24 @@ let uuid = require('uuid');
  * @param {string} name - The command name
  * @param {object} aggregate - The aggregate identity
  * @param {object} payload - The command payload
+ * @param {object} meta - The command meta
  * @constructor {Command}
  */
-function Command(name,aggregate,payload){
-
-    if(!name)
+function Command(name, aggregate, payload, meta) {
+    if (!name)
         throw new Error('The command name cannot be null or empty');
-    if (!payload){
+    if (!meta && payload && aggregate) {
+        meta = payload;
+        payload = aggregate;
+    }
+    if (!meta && !payload) {
         payload = aggregate;
     }
     this.name = name;
     this.id = uuid.v1();
     this.payload = payload || {};
     this.aggregate = aggregate || {};
+    this.meta = meta;
 }
 
 module.exports = Command;
